81cd3393fc0b344eb60e99a18a9805edca03a2c3
UttamUk <33575994+UttamUk@users.noreply.github.com>
2020-12-02 11:12:34 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(window).on('scroll', function () {
+	if ($(window).scrollTop()) {
+		$('.navbar').addClass('blackto');
+	} else {
+		$('.navbar').removeClass('blackto');
+	}
+})
+$(document).ready(function () {
+	$('.navbar-nav>li>a').on('click', function () {
+		$('.navbar-collapse').collapse('hide');
+	});
+});
